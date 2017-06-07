392705ed5a809f1205ea7e1225841a112a6e7cf8
Karthik Bhat A <karthik.bhata7@gmail.com>
2017-06-07 21:24:44 +0530

    random commit

---
+++ namegen.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3.5
+
+import linecache
+import random
+import argparse
+
+def maxlines():
+	with open('/home/karthik/Desktop/karthik_bhata7/Python/random_name/words.txt') as f:
+	    max = sum(1 for _ in f)
+	return max
+
+def getrandomline():
+	line_num = random.randrange(0, max)
+	line = linecache.getline('/home/karthik/Desktop/karthik_bhata7/Python/random_name/words.txt', line_num)
+	line = line.split('\n')[0]
+	return line
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Random filename generator!')
+	parser.add_argument('--create', '-c', help='Create filename', nargs='?', const='')
+	parser.add_argument('--begin', '-b', help='Begin with name', nargs='?', const='', default='')
+	parser.add_argument('--end', '-e', help='End with name', nargs='?', const='', default='')
+	args = parser.parse_args()
+	max = maxlines()
+	name = getrandomline()
+	if args.create is not None:
+		filename = args.begin +  name + args.end + args.create
+		print("Created file: " + filename)
+		with open(filename, "w") as f:
+			pass
+	else:
+		print(name)
